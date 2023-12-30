82d99e9efaea08897a977ef682c27b7a4b02224b
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-12-30 02:48:07 -0300

    fix: showing if task is completed in the checkbox and label

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -6,23 +6,25 @@ import { TaskI } from '../../App';
 interface TaskPropsInterface {
     taskInfo: TaskI,
     onDeleteTask: (taskId: string) => void;
+    onChangeTaskState: (taskId: string) => void; 
 }
-function Task({ taskInfo, onDeleteTask }: TaskPropsInterface) {
+function Task({ taskInfo, onDeleteTask, onChangeTaskState }: TaskPropsInterface) {
 
     const handleDeleteClick = () => {
         const choice = window.confirm("Tem certeza que deseja deletar esta tarefa?");
         choice &&
             onDeleteTask(taskInfo.id);
     }
-
+    
+    const paragraphStyle = taskInfo.isCompleted ? `${styles.paragraph} ${styles.completed}` : styles.paragraph
 
     return (
         <main className={styles.taskCard}>
-            <label className={styles.checkboxLabel}>
-                <input type="checkbox" name="taskStatus" />
+            <label className={styles.checkboxLabel} onClick={() => onChangeTaskState(taskInfo.id)}>
+                <input type="checkbox" name="taskStatus" checked={taskInfo.isCompleted}/>
                 <Check className={styles.checkIcon} size={12} />
             </label>
-            <p className={`${styles.paragraph} ${styles.completed}`}>{taskInfo.title}</p>
+            <p className={paragraphStyle}>{taskInfo.title}</p>
             <button onClick={handleDeleteClick}>
                 <Trash size={16} color="#808080"/>
             </button>  
